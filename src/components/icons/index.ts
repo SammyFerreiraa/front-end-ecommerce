7741abed2345e1eae4339cc5b703a868245886bc
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-18 13:33:59 -0300

    :ok_hand: style: icons components

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CartIcon } from './CartIcon'
+export { default as HeartIcon } from './HeartIcon'
+export { default as MessageIcon } from './MessageIcon'
+export { default as UserIcon } from './UserIcon'
