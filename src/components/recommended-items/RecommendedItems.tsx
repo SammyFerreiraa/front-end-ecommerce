ca1c5eaf2fb4fd9117822995fa8259bbd3e8dbda
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-22 19:20:10 -0300

    :bug: fix: load items mobile left

--- src/components/recommended-items/RecommendedItems.tsx
+++ src/components/recommended-items/RecommendedItems.tsx
@@ -3,8 +3,8 @@ import ItemsRecommended from './ItemsRecommended'
 
 const RecommendedItems = () => {
   return (
-    <div className="flex w-full flex-col items-start justify-center gap-3 px-3 py-5">
-      <h1 className="text-xl font-semibold text-zinc-900 lg:text-2xl lg:leading-loose">
+    <div className="flex w-full flex-col items-center justify-center gap-3 px-3 py-5">
+      <h1 className="w-full text-left text-xl font-semibold text-zinc-900 lg:text-2xl lg:leading-loose">
         Produtos Recomendados
       </h1>
       <ItemsRecommended />
