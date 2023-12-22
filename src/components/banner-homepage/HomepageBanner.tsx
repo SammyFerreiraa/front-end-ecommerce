2e8f7d2bf46267f3d1be850bf3206e864ae74f72
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-22 15:15:10 -0300

    :recycle: refactor: HomepageBanner

--- src/components/banner-homepage/HomepageBanner.tsx
+++ src/components/banner-homepage/HomepageBanner.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Button } from '../ui/button'
 import { getServerSession } from 'next-auth'
 import { nextAuthOption } from '@/app/api/auth/[...nextauth]/route'
+import RightSide from './RightSide'
 
 const HomepageBanner = async () => {
   const session = await getServerSession(nextAuthOption)
@@ -62,27 +62,7 @@ const HomepageBanner = async () => {
       </div>
 
       {/* right */}
-      <div className="hidden h-auto max-w-[200px] flex-col items-center justify-between md:flex md:gap-2 md:text-sm xl:gap-0">
-        <div className="flex w-full flex-col justify-between gap-2 rounded-md bg-sky-100 p-3 text-sm">
-          <div className="flex w-full flex-row items-center justify-center gap-2">
-            <img src="/assets/Avatar.png" className="h-10 w-10" alt="Avatar" />
-            <p className="text">{session?.user?.name}</p>
-            {!session?.user?.name && <p className="text">Oi, vamos começar </p>}
-          </div>
-          <Button className="w-full bg-blue-600 text-white hover:bg-blue-500">
-            Entrar
-          </Button>
-          <Button className="w-full rounded-md bg-white py-2 text-blue-600 hover:bg-gray-100">
-            Registrar
-          </Button>
-        </div>
-        <div className="w-full rounded-md bg-orange-500 p-4 text-white">
-          <p>Ganhe US$ 10 de desconto</p>
-        </div>
-        <div className="w-full rounded-md bg-teal-400 p-4 text-white">
-          <p>Frete Gratis na primeira compra</p>
-        </div>
-      </div>
+      <RightSide name={session?.user?.name} />
     </div>
   )
 }
---
+++ src/components/banner-homepage/RightSide.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from 'react'
+import { Button } from '../ui/button'
+
+type RightSideProps = {
+  name: string | null | undefined
+}
+
+const RightSide = ({ name }: RightSideProps) => {
+  return (
+    <div className="hidden h-auto max-w-[200px] flex-col items-center justify-between md:flex md:gap-2 md:text-sm xl:gap-0">
+      <div className="flex w-full flex-col justify-between gap-3 rounded-md bg-sky-100 p-3 text-sm">
+        <div className="flex w-full flex-row items-center justify-center gap-2">
+          <img src="/assets/Avatar.png" className="h-10 w-10" alt="Avatar" />
+          <p className="text-sm font-bold">{name}</p>
+          {!name && <p className="text">Oi, vamos começar </p>}
+        </div>
+        <Button
+          onClick={() => {
+            window.location.href = '/login'
+          }}
+          className="w-full bg-blue-600 text-white hover:bg-blue-500"
+        >
+          Entrar
+        </Button>
+        <Button className="w-full rounded-md bg-white py-2 text-blue-600 hover:bg-gray-100">
+          Registrar
+        </Button>
+      </div>
+      <div className="w-full rounded-md bg-orange-500 p-4 text-white">
+        <p>Ganhe US$ 10 de desconto</p>
+      </div>
+      <div className="w-full rounded-md bg-teal-400 p-4 text-white">
+        <p>Frete Gratis na primeira compra</p>
+      </div>
+    </div>
+  )
+}
+
+export default RightSide
