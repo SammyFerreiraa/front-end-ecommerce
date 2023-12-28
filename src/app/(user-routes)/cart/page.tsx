e80b0a2c6293b821438d65722fcbe0c515ee666b
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-27 22:46:14 -0300

    :sparkles: feat: products cart mobile

--- src/app/(user-routes)/cart/page.tsx
+++ src/app/(user-routes)/cart/page.tsx
@@ -1,5 +1,7 @@
 import { MaxWidthWrapper } from '@/components'
 import React from 'react'
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import { FaPlus, FaMinus } from 'react-icons/fa6'
 
 const Home = () => {
   return (
@@ -9,8 +11,135 @@ const Home = () => {
           Shopping Cart (3)
         </h1>
       </div>
-      <div>
-        <div></div>
+      <div className="w-full">
+        <div className="w-full">
+          <div className="flex flex-col items-center justify-center">
+            {/* produto */}
+            <div className="flex w-full flex-col gap-5  border-b border-neutral-300 bg-white p-4">
+              <div className="flex flex-row justify-start gap-3">
+                <div className="h-fit w-16 rounded-md border border-neutral-300 bg-white p-1">
+                  <img
+                    src="/images/cloth/1.png"
+                    alt="produto"
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex w-full flex-row justify-between">
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-base text-zinc-900">
+                      T-shirts with multiple colors for men
+                    </h1>
+                    <p className="text-[13px] text-gray-400">
+                      Size: medium, Color: blue
+                    </p>
+                    <p className="text-[13px] text-gray-400">
+                      Seller: Artel Market
+                    </p>
+                  </div>
+                  <div className="cursor-pointer p-2 ">
+                    <BsThreeDotsVertical />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row rounded-md border border-neutral-300">
+                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
+                    <FaMinus />
+                  </div>
+                  <div className="flex items-center justify-center border-x border-neutral-300 px-4 py-2">
+                    <p className="font-bold">2</p>
+                  </div>
+                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
+                    <FaPlus />
+                  </div>
+                </div>
+                <p>R$ 78,99</p>
+              </div>
+            </div>
+            <div className="flex w-full flex-col gap-5 rounded-md border-b border-neutral-300 bg-white p-4">
+              <div className="flex flex-row justify-start gap-3">
+                <div className="h-fit w-16 rounded-md border border-neutral-300 bg-white p-1">
+                  <img
+                    src="/images/cloth/1.png"
+                    alt="produto"
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex w-full flex-row justify-between">
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-base text-zinc-900">
+                      T-shirts with multiple colors for men
+                    </h1>
+                    <p className="text-[13px] text-gray-400">
+                      Size: medium, Color: blue
+                    </p>
+                    <p className="text-[13px] text-gray-400">
+                      Seller: Artel Market
+                    </p>
+                  </div>
+                  <div className="cursor-pointer p-2 ">
+                    <BsThreeDotsVertical />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row rounded-md border border-neutral-300">
+                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
+                    <FaMinus />
+                  </div>
+                  <div className="flex items-center justify-center border-x border-neutral-300 px-4 py-2">
+                    <p className="font-bold">2</p>
+                  </div>
+                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
+                    <FaPlus />
+                  </div>
+                </div>
+                <p>R$ 78,99</p>
+              </div>
+            </div>
+            <div className="flex w-full flex-col gap-5 rounded-md border-b border-neutral-300 bg-white p-4">
+              <div className="flex flex-row justify-start gap-3">
+                <div className="h-fit w-16 rounded-md border border-neutral-300 bg-white p-1">
+                  <img
+                    src="/images/cloth/1.png"
+                    alt="produto"
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex w-full flex-row justify-between">
+                  <div className="flex flex-col p-1">
+                    <h1 className="text-base text-zinc-900">
+                      T-shirts with multiple colors for men
+                    </h1>
+                    <p className="text-[13px] text-gray-400">
+                      Size: medium, Color: blue
+                    </p>
+                    <p className="text-[13px] text-gray-400">
+                      Seller: Artel Market
+                    </p>
+                  </div>
+                  <div className="cursor-pointer p-2 ">
+                    <BsThreeDotsVertical />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row rounded-md border border-neutral-300">
+                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
+                    <FaMinus />
+                  </div>
+                  <div className="flex items-center justify-center border-x border-neutral-300 px-4 py-2">
+                    <p className="font-bold">2</p>
+                  </div>
+                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
+                    <FaPlus />
+                  </div>
+                </div>
+                <p>R$ 78,99</p>
+              </div>
+            </div>
+          </div>
+        </div>
         <div></div>
       </div>
     </MaxWidthWrapper>
