563603b2e2d3b057229ab94d504a7077f38056dc
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-01 20:57:55 -0300

    :ok_hand: style: shadow cart component

--- src/app/(user-routes)/cart/page.tsx
+++ src/app/(user-routes)/cart/page.tsx
@@ -28,7 +28,7 @@ const Home = () => {
       {/* shopping cart */}
       <div className="flex w-full flex-col md:gap-4 lg:flex-row">
         <div className="w-full">
-          <div className="flex flex-col items-center justify-center ">
+          <div className="flex flex-col items-center justify-center lg:shadow-md">
             {/* produto */}
 
             <div className="flex w-full flex-col gap-5  border-b border-neutral-300 bg-white p-4">
@@ -238,79 +238,10 @@ const Home = () => {
                 <p className="flex lg:hidden">R$ 78,99</p>
               </div>
             </div>
-            <div className="flex w-full flex-col gap-5  border-b border-neutral-300 bg-white p-4">
-              <div className="flex flex-row justify-start gap-3">
-                <div className="h-fit w-16 rounded-md border border-neutral-300 bg-white p-1">
-                  <img
-                    src="/images/cloth/1.png"
-                    alt="produto"
-                    className="w-full"
-                  />
-                </div>
-                <div className="flex w-full flex-row justify-between">
-                  <div className="flex flex-col p-1">
-                    <h1 className="text-base text-zinc-900">
-                      T-shirts with multiple colors for men
-                    </h1>
-                    <p className="text-[13px] text-gray-400">
-                      Size: medium, Color: blue
-                    </p>
-                    <p className="text-[13px] text-gray-400">
-                      Seller: Artel Market
-                    </p>
-                  </div>
-                  <div className="flex cursor-pointer p-2 lg:hidden">
-                    <BsThreeDotsVertical />
-                  </div>
-                  <p className="hidden p-2 lg:flex">R$ 78,99</p>
-                </div>
-              </div>
-              <div className="flex flex-row items-center justify-between">
-                <div className="hidden gap-3 lg:flex">
-                  <Button className="border border-gray-300 bg-transparent text-center text-[13px] font-medium text-red-600 hover:bg-red-600 hover:text-white">
-                    Remove
-                  </Button>
-                  <Button className="border border-gray-300 bg-transparent text-center text-[13px] font-medium text-blue-600 hover:bg-blue-600 hover:text-white">
-                    Save for Later
-                  </Button>
-                </div>
-                <div className="flex flex-row rounded-md border border-neutral-300 lg:hidden">
-                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
-                    <FaMinus />
-                  </div>
-                  <div className="flex items-center justify-center border-x border-neutral-300 px-4 py-2">
-                    <p className="font-bold">2</p>
-                  </div>
-                  <div className="flex cursor-pointer items-center justify-center p-2 px-3">
-                    <FaPlus />
-                  </div>
-                </div>
-                <div>
-                  <Select defaultValue="1">
-                    <SelectTrigger className="mr-3 hidden shadow lg:flex">
-                      Quant: {'  '}
-                      <SelectValue placeholder="" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="1">{'  '}1</SelectItem>
-                      <SelectItem value="2">{'  '}2</SelectItem>
-                      <SelectItem value="3">{'  '}3</SelectItem>
-                      <SelectItem value="4">{'  '}4</SelectItem>
-                      <SelectItem value="5">{'  '}5</SelectItem>
-                      <SelectItem value="6">{'  '}6</SelectItem>
-                      <SelectItem value="7">{'  '}7</SelectItem>
-                      <SelectItem value="8">{'  '}8</SelectItem>
-                      <SelectItem value="9">{'  '}9</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <p className="flex lg:hidden">R$ 78,99</p>
-              </div>
-            </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-start gap-3">
-          <div className="hidden flex-col gap-2 rounded-md bg-white p-5 lg:flex">
+        <div className="flex h-fit flex-col items-center justify-start gap-3">
+          <div className="hidden flex-col gap-2 rounded-md bg-white p-5 lg:flex lg:shadow-md">
             <p className="text-base font-normal text-neutral-600">
               Have a coupon?
             </p>
@@ -324,7 +255,7 @@ const Home = () => {
               </Button>
             </div>
           </div>
-          <div className="w-full rounded-md bg-white p-4 py-5 lg:w-[280px]">
+          <div className="w-full rounded-md bg-white p-4 py-5 lg:w-[280px] lg:shadow-md">
             <div className="flex flex-col gap-2">
               <div className="flex flex-row items-center justify-between ">
                 <p className="text-gray-400">Items (3):</p>
