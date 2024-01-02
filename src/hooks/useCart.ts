3576f749163e695be60734277724ce8c0dc48303
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-02 16:17:37 -0300

    :sparkles: feat: setQuantity

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -3,9 +3,20 @@ import { ProductCartProps } from '../@types'
 interface UseCartProps {
   cart: ProductCartProps[]
   setCart: (cart: ProductCartProps[]) => void
+  setQuantity: (code: string, quantity: number) => void
 }
 
 export const useCart = create<UseCartProps>((set) => ({
   cart: [],
   setCart: (cart) => set({ cart }),
+  setQuantity: (code, quantity) => {
+    set((state) => ({
+      cart: state.cart.map((product) => {
+        if (product.code === code) {
+          return { ...product, quantity }
+        }
+        return product
+      }),
+    }))
+  },
 }))
