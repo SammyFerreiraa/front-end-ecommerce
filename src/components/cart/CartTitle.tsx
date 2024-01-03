d0ea7fc7434a929525b9e24c09337903a595d86a
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-03 10:43:26 -0300

    :recycle: refactor: title with profile route

--- src/components/cart/CartTitle.tsx
+++ src/components/cart/CartTitle.tsx
@@ -1,12 +1,18 @@
 import { nextAuthOption } from '@/app/api/auth/[...nextauth]/route'
+import axios from 'axios'
 import { getServerSession } from 'next-auth'
 import React from 'react'
 
 const CartTitle = async () => {
   const session = await getServerSession(nextAuthOption)
+  const res = await axios.get('http://localhost:3000/profile', {
+    headers: {
+      Authorization: `Bearer ${session?.token}`,
+    },
+  })
   return (
     <h1 className="text-lg font-semibold text-zinc-900 md:text-2xl">
-      Shopping Cart ({session?.user?.cart?.products?.length})
+      Shopping Cart ({res.data.cart.products.length})
     </h1>
   )
 }
