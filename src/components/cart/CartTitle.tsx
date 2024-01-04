88059be7148cc8becbd709e287f2521aa7b88d0f
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-04 17:59:31 -0300

    :recycle: refactor: length products from useCart

--- src/components/cart/CartTitle.tsx
+++ src/components/cart/CartTitle.tsx
@@ -1,18 +1,13 @@
-import { nextAuthOption } from '@/app/api/auth/[...nextauth]/route'
-import axios from 'axios'
-import { getServerSession } from 'next-auth'
+'use client'
+
+import { useCart } from '@/hooks/useCart'
 import React from 'react'
 
-const CartTitle = async () => {
-  const session = await getServerSession(nextAuthOption)
-  const res = await axios.get('http://localhost:3000/profile', {
-    headers: {
-      Authorization: `Bearer ${session?.token}`,
-    },
-  })
+const CartTitle = () => {
+  const cart = useCart((state) => state.cart)
   return (
     <h1 className="text-lg font-semibold text-zinc-900 md:text-2xl">
-      Shopping Cart ({res.data.cart.products.length})
+      Shopping Cart ({cart.products.length})
     </h1>
   )
 }
