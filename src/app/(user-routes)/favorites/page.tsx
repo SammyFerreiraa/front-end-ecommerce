2b466ae97738dc21daf5c5735aef6c4231537877
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-05 13:25:27 -0300

    :sparkles: feat: favorite route

---
+++ src/app/(user-routes)/favorites/page.tsx
@@ -0,0 +1,12 @@
+import { MaxWidthWrapper } from '@/components'
+import React from 'react'
+
+const page = () => {
+  return (
+    <MaxWidthWrapper className="flex flex-col items-center gap-3 pb-11 md:px-[50px] lg:px-12 xl:px-0">
+      <div></div>
+    </MaxWidthWrapper>
+  )
+}
+
+export default page
