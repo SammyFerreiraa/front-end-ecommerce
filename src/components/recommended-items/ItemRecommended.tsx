97301b0928cfcadd3a9cd6e80723fb73b8accfa4
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-05 11:15:42 -0300

    :sparkles: feat: rating and favorite button

--- src/components/recommended-items/ItemRecommended.tsx
+++ src/components/recommended-items/ItemRecommended.tsx
@@ -8,7 +8,25 @@ const ItemRecommended = ({ name, image, price }: ItemRecommendedProps) => {
         <img src={image} alt="camisa" />
       </div>
       <div className="flex flex-col gap-1">
-        <p className="font-medium leading-snug text-zinc-900">{price}</p>
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-col items-start justify-center gap-1">
+            <p className="font-medium leading-snug text-zinc-900">
+              R$ {'   '}
+              {price}
+            </p>
+            <div className="flex flex-row items-center gap-1.5">
+              <img src="/images/rating/4.png" alt="4" className="w-16" />
+              <p className="text-sm text-amber-500">4.0</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center rounded-md border-2 border-gray-300 p-2">
+            <img
+              src="/images/icons/favorite_border.png"
+              alt="fav"
+              className="h-5 w-5"
+            />
+          </div>
+        </div>
         <p className="line-clamp-2 text-[13px] text-gray-400">{name}</p>
       </div>
     </div>
