fc052b57de2ed64aa29ec3b1436de3c47b14bd3a
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-11 13:06:39 -0300

    :lipstick: feat: CheckIcon component

---
+++ src/components/icons/CheckIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CheckIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.79508 15.8749L4.62508 11.7049L3.20508 13.1149L8.79508 18.7049L20.7951 6.70492L19.3851 5.29492L8.79508 15.8749Z"
+        fill="#00B517"
+      />
+    </svg>
+  )
+}
+
+export default CheckIcon
